bf3a86f21eb9439a91dc6aebe978420cd01121f3
Koussay Hadded <koussay.hadded@esprit.tn>
2023-02-17 18:11:45 +0000

    Account Entity

---
+++ src/Entity/Account.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+class Account
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $NomComplet = null;
+
+    #[ORM\Column]
+    private ?int $NumTel = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Sexe = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $DateNaiss = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Adresse = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Ville = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $CarteCIN = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomComplet(): ?string
+    {
+        return $this->NomComplet;
+    }
+
+    public function setNomComplet(string $NomComplet): self
+    {
+        $this->NomComplet = $NomComplet;
+
+        return $this;
+    }
+
+    public function getNumTel(): ?int
+    {
+        return $this->NumTel;
+    }
+
+    public function setNumTel(int $NumTel): self
+    {
+        $this->NumTel = $NumTel;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->Sexe;
+    }
+
+    public function setSexe(string $Sexe): self
+    {
+        $this->Sexe = $Sexe;
+
+        return $this;
+    }
+
+    public function getDateNaiss(): ?\DateTimeInterface
+    {
+        return $this->DateNaiss;
+    }
+
+    public function setDateNaiss(\DateTimeInterface $DateNaiss): self
+    {
+        $this->DateNaiss = $DateNaiss;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->Ville;
+    }
+
+    public function setVille(string $Ville): self
+    {
+        $this->Ville = $Ville;
+
+        return $this;
+    }
+
+    public function getCarteCIN(): ?string
+    {
+        return $this->CarteCIN;
+    }
+
+    public function setCarteCIN(string $CarteCIN): self
+    {
+        $this->CarteCIN = $CarteCIN;
+
+        return $this;
+    }
+}
---
+++ src/Form/AccountType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\App\Form\FileType;
+use Symfony\Component\Form\Extension\Core\Type\FileType as TypeFileType;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('NomComplet')
+            ->add('NumTel')
+            ->add('Email')
+            ->add('Sexe', ChoiceType::class, array(
+                'choices' => array(
+                    'Male' => 'male',
+                    'Female' => 'female',
+                    'Other' => 'other',
+                ),
+                'expanded' => false,
+                'multiple' => false,
+            ))
+            ->add('DateNaiss', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('Adresse')
+            ->add('Ville')
+            ->add('CarteCIN', TypeFileType::class, ['attr' => ['class' => 'custom-file-input'],
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid ImageFile',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
