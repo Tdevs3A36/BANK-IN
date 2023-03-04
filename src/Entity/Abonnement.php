ea04ecd1c4a96629bbf327293c2d50bfae109e96
Koussay Hadded <koussay.hadded@esprit.tn>
2023-03-04 20:59:57 +0000

    Abonnement update

--- src/Entity/Abonnement.php
+++ src/Entity/Abonnement.php
@@ -52,7 +52,13 @@ class Abonnement
     
     
 
-    
+    /**
+     * @return Collection<int, Service>
+     */
+    public function getServices(): Collection
+    {
+        return $this->services;
+    }
 
     
     public function __toString(): string{
